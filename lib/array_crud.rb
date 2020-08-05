55102502162b3a02c4a821cce03c17e1bb9a2b41
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-05 01:14:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-   array = ["apples", "kiwi", "ginger", "mango"]
+   ["apples", "kiwi", "ginger", "mango"]
 end
 
 def add_element_to_end_of_array(array, element)
-   array.push("arrays!")
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-   array.unshift("wow")
+   array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
